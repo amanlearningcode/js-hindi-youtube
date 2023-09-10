083be3f44d21dafcedbc4dcbd352de345c0bd910
amanlearningcode <144514363+amanlearningcode@users.noreply.github.com>
2023-09-10 04:26:59 +0000

    variables

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,15 @@
-const accountID = 1234567
+const accountId = 1234567
+let accountEmail = "aman@google.com"
+var accountPassword = "1234"
+accountCity = "Varanasi"
+let accountState
+
+//accountId = 2 // not allowed
+
+accountEmail = "maurya@google.com"
+accountPassword = "21212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
